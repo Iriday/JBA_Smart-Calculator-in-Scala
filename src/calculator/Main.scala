1d1e68cce15ed46032f1400c1269e23b1609ab8a
Igor <GI_Something@protonmail.com>
2020-12-21 10:10:50 +0200

    feat: Add basic structure/ support addition

--- src/calculator/Main.scala
+++ src/calculator/Main.scala
@@ -1,5 +1,21 @@
 package calculator
 
+import scala.io.StdIn.readLine
+
 object Main extends App {
-  println("Hello World!")
+  while (true) {
+    val in = readLine().trim
+
+    if (in.nonEmpty) {
+      if (in.equalsIgnoreCase("/exit")) {
+        println("Bye!")
+        System.exit(0)
+      } else if (in.equalsIgnoreCase("/help")) {
+        println("The program calculates the sum of numbers.")
+      } else {
+        println(in.split("\\s+").map(v => v.toInt).sum)
+      }
+
+    }
+  }
 }
