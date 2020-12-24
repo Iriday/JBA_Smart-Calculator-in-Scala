fcc7f4feb0560f73119b8c1a3bf8da57d49a3826
Igor <GI_Something@protonmail.com>
2020-12-24 11:16:45 +0200

    feat: Support variables

--- src/calculator/Main.scala
+++ src/calculator/Main.scala
@@ -1,40 +1,49 @@
 package calculator
 
 import scala.io.StdIn.readLine
+import scala.collection.mutable
 import scala.collection.mutable.ArrayBuffer
 
 object Main extends App {
-  run()
+  private val variables: mutable.HashMap[String, String] = mutable.HashMap()
 
-  def run() {
+  private val rLetters = "[a-zA-Z]+".r
+  private val rNumWithOptionalSign = "[-+]?[0-9]+".r
+  private val rWhitespaceBetweenAlphanum = ".*[0-9a-zA-Z]\\s+[0-9a-zA-Z].*".r
+  private val rExpression = "[-+]*([0-9]+|[a-zA-Z]+)([-+]+([0-9]+|[a-zA-Z]+))*".r
+
+  run(variables)
+
+  def run(variables: mutable.HashMap[String, String]) {
     while (true) {
       val in = getInput
 
       if (in.nonEmpty) {
-        val errorMsg = checkInput(in)
-        if (errorMsg != null) {
-          output(errorMsg)
-        } else if (in.equals("/exit")) {
-          output("Bye!")
-          System.exit(0)
-        } else if (in.equals("/help")) {
-          output("Example: 2+2 -2 ---2 +-2")
+        if (in.startsWith("/")) {
+          executeCommand(in)
+        } else if (in.contains("=")) {
+          val errorMsg = checkAssignment(in, variables)
+          if (errorMsg != null) output(errorMsg) else addVariable(in)
         } else {
-          output(solveExpression(formatExpression(in)))
+          val errorMsg = checkExpression(in, variables)
+          output(if (errorMsg != null) errorMsg else solveExpression(formatExpression(in, variables)))
         }
       }
     }
   }
 
-  def getInput: String = readLine().trim.toLowerCase
+  def getInput: String = readLine().trim
 
   def output(data: Any): Unit = println(data)
 
-  // removes whitespaces, and merges operators
-  def formatExpression(expression: String): ArrayBuffer[String] = {
+  /**
+   * Remove whitespaces, merge operators (- and +), replace vars names with values.
+   */
+  def formatExpression(expression: String, variables: mutable.HashMap[String, String]): ArrayBuffer[String] = {
     def mergeOperators(a: Char, b: Char): Char = if (a == '-' && b == '-' || a == '+' && b == '+') '+' else '-'
 
-    val expr = expression.replace(" ", "")
+    var expr = expression.replace(" ", "")
+    variables.foreach(v => expr = expr.replace(v._1, v._2))
     val exprArr = ArrayBuffer.empty[String]
 
     var i = 0
@@ -86,23 +95,52 @@ object Main extends App {
     result
   }
 
-  /**
-   * @param in input
-   * @return null if input is correct else error msg
-   */
-  def checkInput(in: String): String = {
-    // general
-    if (in.isEmpty) return null
-    // command
-    if (in.startsWith("/")) {
-      if (in.equals("/help") || in.equals("/exit")) return null
-      else return "Unknown command"
+  private def executeCommand(in: String) {
+    if (in.equals("/exit")) {
+      output("Bye!")
+      System.exit(0)
+    } else if (in.equals("/help")) {
+      output("Examples: a = 2; b=3; a+2 -2 ---b +-2")
+    } else {
+      output("Unknown command")
     }
-    // expression
-    if (".*[0-9]\\s+[0-9].*".r.matches(in)) return "Invalid expression"
+  }
 
-    val in2 = in.replace(" ", "") // remove whitespaces to simplify regex
-    if (!"[-+]*[0-9]+([-+]+[0-9]+)*".r.matches(in2)) return "Invalid expression"
+  private def addVariable(in: String) {
+    val v = in.split(" *= *")
+    variables += v(0) -> variables.getOrElse(v(1), v(1))
+  }
+
+  // Input check
+
+  /**
+   * @return null if expression is correct else error msg
+   */
+  private def checkExpression(expr: String, vars: mutable.HashMap[String, String]): String = {
+    if (rWhitespaceBetweenAlphanum.matches(expr)) return "Invalid expression"
+
+    val expr2 = expr.replace(" ", "") // remove whitespaces to simplify regex
+    if (!rExpression.matches(expr2)) return "Invalid expression"
+
+    val expr3 = expr2.split("[^a-zA-Z]+")
+    expr3.foreach(v => if (!vars.contains(v)) return "Unknown variable")
+
+    null
+  }
+
+  /**
+   * @return null if assignment is correct else error msg
+   */
+  private def checkAssignment(a: String, vars: mutable.HashMap[String, String]): String = {
+    val a2 = a.split(" *= *")
+
+    if (a2.length != 2) return "Invalid assignment"
+
+    if (!rLetters.matches(a2(0))) return "Invalid identifier"
+
+    if (!rLetters.matches(a2(1)) && !rNumWithOptionalSign.matches(a2(1))) return "Invalid assignment"
+
+    if (rLetters.matches(a2(1)) && !vars.contains(a2(1))) return "Unknown variable"
 
     null
   }
